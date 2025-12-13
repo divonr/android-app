28b345a7665f1a99fbece9ad02be611de0e3c908
divonr <divon.rozen@mail.huji.ac.il>
2025-12-14 00:19:03 +0200

    some more fixes, Google thoughts still

--- app/src/main/java/com/example/ApI/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/example/ApI/ui/screen/ChatScreen.kt
@@ -56,6 +56,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.SpanStyle
